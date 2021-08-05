e31eb098eaf28ad7a5dad4bf1b24e37e8f37009c
mariamiiovashvili <87204395+mariamiiovashvili@users.noreply.github.com>
2021-08-06 01:23:43 +0400

    js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 1.
 let cars = [ "audi","bmw","lexus", "volkswagen", "porsche","audi"];
-for (let x=1; x < 6; x++)
-console.log(cars[x]);
+  for (let x=1; x < 6; x++)
+     console.log(cars[x]);
 
 
 
 2.
 let names = [ "nika", "beqa" , "sandro" , "ia" , "nia", "marishka"];
-for (let x=0; x < names.length; x++) {
+ for (let x=0; x < names.length; x++) {
     if (names [x].length > 3) {
         console.log(names[x]);
     }
@@ -16,7 +16,7 @@ for (let x=0; x < names.length; x++) {
 
 3.
 let number = [1,4,5,6,9,11,16,17,25,32,43,56,65,66]
-for (let item of number) {
+ for (let item of number) {
     if (item % 2 == 0) {
         console.log(item);
     }
@@ -32,7 +32,7 @@ for (let item of number) {
 4.
 let numbers=[ 15,67,100,5,7,[80,500]]
 
-console.log(numbers[5][1]);
+   console.log(numbers[5][1]);
 
 5.
 var person = {
@@ -42,25 +42,25 @@ var person = {
     eyecolor: 'blue'
 }
 
-console.log(person.eyecolor);
-console.log(person.age);
+    console.log(person.eyecolor);
+    console.log(person.age);
 
 6. 
 let today = [5,'august',2021]
-for ( let x of today)
-console.log(x);
+   for ( let x of today)
+      console.log(x);
 
 7.
 let items = ['sandro','nini','ika','nia','nika']
-if (item = 5 && items[4]==='nika')
+    if (item = 5 && items[4]==='nika')
 
-console.log('მასივის რაოდენობა: 5 და ბოლო ელემენტია ნიკა');
+         console.log('მასივის რაოდენობა: 5 და ბოლო ელემენტია ნიკა');
 
-if (item =7 || item[0] === 'sandro' )
-console.log('მასივის რაოდენობა: 7 ან პირველი ელემენტია სანდრო');
+    if (item =7 || item[0] === 'sandro' )
+         console.log('მასივის რაოდენობა: 7 ან პირველი ელემენტია სანდრო');
 
 8.
 let x = 6;
 let y = '6'; 
-console.log(6==='6'); 
+    console.log(6==='6'); 
 //  false რადგან არ ხდება ტიპების კონვერტაცია , სტრინგი არ გადადის რიცხვში 3 ტოლობის დროს
